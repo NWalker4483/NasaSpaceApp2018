26ed124c6f2f78df6ef68ee94f8e6ca9c49b2ac6
Nile Walker <nile@Niles-MacBook-Pro.local>
2018-10-20 16:08:58 -0400

    Added Test code for Sensor Node Handler

--- NodeStuff.py
+++ NodeStuff.py
@@ -3,28 +3,34 @@ import time
 import socket 
 
 class SensorNodeHandler(Thread):
-    def __init__(self,socket, UDP_IP, UDP_PORT, port,latitude = 0,longitude = 0):
+    def __init__(self, UDP_IP, UDP_PORT,latitude = 0,longitude = 0, Socket = None):
         Thread.__init__(self)
         self.ip = UDP_IP
         self.port = UDP_PORT
         self.latitude = latitude
         self.longitude = longitude
-        self.socket=socket
+        #self.socket=Socket
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind((UDP_IP, UDP_PORT))
 
     def run(self):
         while True:
             time.sleep(0.1)
-            data = self.sock.recvfrom(1024)	
-            line = data.split(',')
+            data, _ = self.sock.recvfrom(1024)	
+            
+            line = data.strip().decode('ascii').split(',')
+            print(line)
             if len(line) == 3:
+                print(line)
                 #flowstr = str(self.flow[i])
                 #flowstr = flowstr.replace("{","")
                 #flowstr = flowstr.replace("}","")
                 #flowstr = flowstr.replace('"',"")
-                self.socket.emit("geo_update",
-                            {"data":line},
-                            namespace="/")
+                #self.socket.emit("geo_update",
+                            #{"data":line},
+                            #namespace="/")
             else:
-                print("received incomplete TCP packet from android IMU")
+                print("received incomplete UCP packet from android IMU")
+if __name__ == "__main__":
+    Test = SensorNodeHandler("192.168.43.14",5000)
+    Test.run()
--- test_flask_heat.py
+++ test_flask_heat.py
@@ -23,6 +23,9 @@ def hello():
     data = [[random.randint(0,1000),random.randint(0,600),random.randint(0,20)] for i in range(800)]
     return render_template(
         'index.html',data=data)
- #thread.flow[message["motor"]]=message["value"]
+@socketio.on("joystick", namespace="/test")    
+def steering(message):
+	print(message)
+ #thread.flow[message["motor"]]=message["value"]thon 
 if __name__ == "__main__":
     socketio.run(app,host="0.0.0.0",debug=True)
